511ec83d05803421af81576252aabd6c3faf145d
Kevin <krestrepobedoya@gmail.com>
2025-03-08 00:07:14 -0500

    deploy8

--- index.js
+++ index.js
@@ -5,15 +5,26 @@ const comprasRoute = require("./routes/comprasRoutes");
 const abonoRoute = require("./routes/abonoRoutes");
 const cors = require("cors");
 
+const corsOptions = {
+    origin: [
+        'http://26.241.225.40:3001',
+        'http://localhost:3002',
+        'http://localhost:3001',
+        'https://effervescent-florentine-6f5672.netlify.app'
+        // Agrega aquí otros orígenes permitidos
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'Accept'
+    ],
+    credentials: true,
+    maxAge: 86400 // 24 horas
+};
 
-app.use(cors(
-    {
-        origin: ["http://26.241.225.40:3001",
-        "http://localhost:3001/","http://localhost:3001"],  
-        methods: "GET,POST,PUT,DELETE",
-        allowedHeaders: "Content-Type, Authorization"
-    }
-));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
@@ -23,6 +34,6 @@ app.use("/abonos", abonoRoute);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
-    });
-    
+});
+
 
